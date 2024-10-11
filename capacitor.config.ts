6a8cdd78998d7044ad71280b5257d5ea3afab113
dev-Kilian <kilianrs26@gmail.com>
2024-10-11 12:08:27 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'guitarShop',
+  webDir: 'www'
+};
+
+export default config;
